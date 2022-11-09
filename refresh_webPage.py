3b55c8489f8a0ad2e287ecd6d71f97aff3809c63
sheikhshakibhosaain <sheikhshakibpro@gmail.com>
2022-11-09 20:44:41 +0600

    comments added

--- refresh_webPage.py
+++ refresh_webPage.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
-#from selenium.common.keys import Keys
 
-#driver = webdriver.Firefox()
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-driver.get('https://github.com/sheikhshakibhossain')
+# select path of chrome driver
+driver = webdriver.Chrome('/usr/local/bin/chromedriver') 
+driver.get('https://github.com/sheikhshakibhossain') # url
 
+# refresh the page after 1 second
 while True:
     time.sleep(1)
     driver.refresh()
