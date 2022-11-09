000d1014e59c634babc3f2a4c002fa1d3da3b96b
sheikhshakibhosaain <sheikhshakibpro@gmail.com>
2022-11-09 21:48:46 +0600

    added chatBot

--- chatBot.py
+++ chatBot.py
@@ -1,20 +1,22 @@
-import time
+import os, time
 import pyautogui as bot
 
-print("Starting attack in: ")
 i = 5
 while(i>0):
-    print(i)
+    print("Starting attack in: " + str(i))
     time.sleep(1)
+    os.system('clear')
     i -= 1
 
 babeName = "my queen"
 mesg = "I love you " + babeName
-wait = 5 # seconds
-count = 5
+wait = 3 # seconds
+count = 1
+
 i = 0
 while (i < count):
     bot.typewrite(mesg)
     bot.press("enter")
     i += 1
     time.sleep(wait)
+print("Done...")
